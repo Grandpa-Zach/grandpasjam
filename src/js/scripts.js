d057e8d9e1999deb658ab27a790edb3a123d672e
Eric Lesinski <18151674+ericlesinski@users.noreply.github.com>
2025-01-02 12:14:11 -0500

    Update scripts.js
    UNDO

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,26 +1,2 @@
-/*!
-* Start Bootstrap - One Page Wonder v6.0.5 (https://startbootstrap.com/theme/one-page-wonder)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
-*/
 // This file is intentionally blank
-// Use this file to add JavaScript to your project
-
-let slideIndex = 0;
-showSlides(slideIndex);
-                          
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slides");
-  if (n >= slides.length) { slideIndex = 0 }
-  if (n < 0) { slideIndex = slides.length - 1 }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
-  slides[slideIndex].style.display = "block"; 
-}
-                    
+// Use this file to add JavaScript to your project      
